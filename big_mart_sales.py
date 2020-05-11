301d8a807cc009415298b635a8a6695409e41934
prasun2106 <prasunk077@gmail.com>
2020-05-11 11:38:32 +0530

    initial predictions made

--- big_mart_sales.py
+++ big_mart_sales.py
@@ -88,7 +88,7 @@
 
 # ## * Model Building
 
-# In[102]:
+# In[1]:
 
 
 import pandas as pd
@@ -102,62 +102,62 @@ import nltk
 # ### * Exploratory Data Analysis Steps
 # 1. Variable Identification
 
-# In[103]:
+# In[2]:
 
 
 train = pd.read_csv('Train_UWu5bXk.txt', sep = ',')
 
 
-# In[104]:
+# In[3]:
 
 
 test = pd.read_csv('Test_u94Q5KV.txt', sep = ',')
 
 
-# In[105]:
+# In[4]:
 
 
 print(train.shape)
 print(test.shape)
 
 
-# In[106]:
+# In[5]:
 
 
 train.head()
 
 
-# In[107]:
+# In[6]:
 
 
 test.head()
 
 
-# In[108]:
+# In[7]:
 
 
 train.isnull().sum(axis = 1).sort_values(ascending = False)
 
 
-# In[109]:
+# In[8]:
 
 
 train.isnull().sum(axis = 0).sort_values(ascending = False)
 
 
-# In[110]:
+# In[9]:
 
 
 data = pd.concat([train, test], ignore_index=True)
 
 
-# In[111]:
+# In[10]:
 
 
 data.info()
 
 
-# In[112]:
+# In[11]:
 
 
 data.nunique().sort_values()
@@ -176,13 +176,13 @@ data.nunique().sort_values()
 # ## * Exploratory Data Analysis
 # 2. Data Cleaning and Missing Values Treatment
 
-# In[113]:
+# In[12]:
 
 
 data.columns
 
 
-# In[114]:
+# In[13]:
 
 
 for col in ['Item_Fat_Content', 'Item_Type', 'Outlet_Identifier','Outlet_Establishment_Year', 'Outlet_Size', 'Outlet_Location_Type',
@@ -190,7 +190,7 @@ for col in ['Item_Fat_Content', 'Item_Type', 'Outlet_Identifier','Outlet_Establi
     print(col, data[col].unique())
 
 
-# In[115]:
+# In[14]:
 
 
 data.Item_Fat_Content.unique()
@@ -198,7 +198,7 @@ data.Item_Fat_Content.unique()
 
 # We have to merge low fat, LF with Low Fat and reg with 'Regular'.
 
-# In[116]:
+# In[15]:
 
 
 data['Item_Fat_Content'] = data['Item_Fat_Content'].replace(['low fat', 'LF'], 'Low Fat')
@@ -206,14 +206,14 @@ data['Item_Fat_Content'] = data['Item_Fat_Content'].replace(['reg'], 'Regular')
 data.Item_Fat_Content.nunique()
 
 
-# In[117]:
+# In[16]:
 
 
 for col in data[['Item_Fat_Content','Item_Type','Outlet_Size','Outlet_Type', 'Outlet_Location_Type' ]]:
     print(data[col].unique())
 
 
-# In[118]:
+# In[17]:
 
 
 data.isnull().sum()
@@ -227,7 +227,7 @@ data.isnull().sum()
 # 3. Since, Outlet_Size has a lot of missing values, we need to decide whether we can drop this variable entirely.
 # 4. We will impute Item_Weight with the mean of each item identifier
 
-# In[119]:
+# In[18]:
 
 
 data[['Item_Identifier','Item_Weight']].drop_duplicates().dropna()['Item_Identifier'].value_counts()
@@ -235,7 +235,7 @@ data[['Item_Identifier','Item_Weight']].drop_duplicates().dropna()['Item_Identif
 
 # The above result shows that each Identifier is mapped to only one weight. So we can safely replace nan with the item weights mapped to each item identifiers
 
-# In[124]:
+# In[19]:
 
 
 # get average weights for each identifier
@@ -243,13 +243,13 @@ avg_weight = data[['Item_Identifier','Item_Weight']].groupby('Item_Identifier').
 data = data.merge(avg_weight, how = 'left', on = 'Item_Identifier').drop('Item_Weight_x', axis = 1).rename(columns = {'Item_Weight_y':'Item_Weight'})
 
 
-# In[ ]:
+# In[20]:
 
 
 # imputing outlet size based on the 
 
 
-# In[136]:
+# In[21]:
 
 
 print(data[['Outlet_Size','Outlet_Identifier']].drop_duplicates())
@@ -262,7 +262,7 @@ print(data[['Outlet_Size','Outlet_Type']].drop_duplicates())
 # * Grocery store is Nan or small, and Supermarket Type 1 is mapped to all possible values (small, medium, high, nan)
 # * We can impute the missing values based on the mode of outlet size in each category of outlet_type
 
-# In[170]:
+# In[22]:
 
 
 mode_outlet_size = data.groupby(by = 'Outlet_Type').agg(lambda x: x.value_counts().index[0])['Outlet_Size']
@@ -270,7 +270,7 @@ mode_outlet_size = mode_outlet_size.reset_index()
 print(mode_outlet_size)
 
 
-# In[176]:
+# In[23]:
 
 
 # Imputing missing outlet size
@@ -279,7 +279,7 @@ data = data.drop('Outlet_Size_x', axis = 1)
 data.rename(columns = {'Outlet_Size_y':'Outlet_Size'}, inplace = True)
 
 
-# In[177]:
+# In[24]:
 
 
 data.isnull().sum()
@@ -288,25 +288,25 @@ data.isnull().sum()
 # ## * Exploratory Data Analysis
 # 3. Univariate Analysis
 
-# In[181]:
+# In[25]:
 
 
 sns.distplot(data.Item_Weight)
 
 
-# In[183]:
+# In[26]:
 
 
 sns.countplot(data.Item_Fat_Content)
 
 
-# In[184]:
+# In[27]:
 
 
 sns.distplot(data.Item_Visibility, bins = 10)
 
 
-# In[214]:
+# In[28]:
 
 
 plt.figure(figsize= [25,12])
@@ -316,7 +316,7 @@ chart.set_xticklabels(chart.get_xticklabels(), rotation = 40)
 plt.show()
 
 
-# In[215]:
+# In[29]:
 
 
 sns.distplot(data.Item_MRP)
@@ -330,13 +330,13 @@ sns.distplot(data.Item_MRP)
 
 # ### 1. continuous and continuous
 
-# In[218]:
+# In[30]:
 
 
 sns.scatterplot(x = data.Item_Weight, y = data.Item_Visibility)
 
 
-# In[217]:
+# In[31]:
 
 
 sns.pairplot(data)
@@ -345,19 +345,19 @@ sns.pairplot(data)
 # ### 2. Categorical and continuous
 # 
 
-# In[219]:
+# In[32]:
 
 
 sns.boxplot(x = data.Item_Fat_Content, y = data.Item_Outlet_Sales)
 
 
-# In[220]:
+# In[33]:
 
 
 sns.boxplot(x = data.Item_Fat_Content, y = data.Item_MRP)
 
 
-# In[242]:
+# In[34]:
 
 
 sns.boxplot(x = data.Item_Fat_Content, y = data.Item_Visibility)
@@ -369,7 +369,7 @@ sns.boxplot(x = data.Item_Fat_Content, y = data.Item_Visibility)
 
 
 
-# In[224]:
+# In[35]:
 
 
 def boxcompare(categorical, continuous):
@@ -378,19 +378,19 @@ def boxcompare(categorical, continuous):
     
 
 
-# In[225]:
+# In[36]:
 
 
 boxcompare('Item_Type', 'Item_MRP')
 
 
-# In[226]:
+# In[37]:
 
 
 boxcompare('Item_Type', 'Item_Outlet_Sales')
 
 
-# In[241]:
+# In[38]:
 
 
 
@@ -413,28 +413,302 @@ for col in [ 'Item_Fat_Content', 'Item_Type',
 # 1. Data Cleaning - already done
 # 2. Encoding
 
-# In[254]:
+# In[39]:
 
 
 categorical_columns = [col for col in data.columns if data[col].dtype == 'object']
 print(categorical_columns)
 
 
-# In[267]:
+# In[40]:
 
 
 one_hot = [col for col in categorical_columns if col not in ['Item_Identifier','Outlet_Identifier']]
 data = pd.get_dummies(data, columns = one_hot)
 
 
-# In[268]:
+# In[41]:
 
 
 data.info()
 
 
-# ### Preprocessing
-# 3. Outlier Detection
+# # Making Initial Predictions
+# Without any further data wrangling, let's make prediction and see how much accuracy are we getting. After making prediction we will follow other steps such as outlier detection, feature engineering, etc.
+
+# In[87]:
+
+
+# split into train and test set
+train = data[~data['Item_Outlet_Sales'].isnull()]
+test = data[data['Item_Outlet_Sales'].isnull()]
+
+
+# In[86]:
+
+
+# using mean to predict
+avg_sales = train['Item_Outlet_Sales'].mean()
+submission_mean = test[['Item_Identifier','Outlet_Identifier']]
+submission_mean['Item_Outlet_Sales'] = avg_sales
+submission_mean.to_csv('submission_mean.csv', index = False)
+
+
+# In[116]:
+
+
+# Making a Pipeline
+target = 'Item_Outlet_Sales'
+id_col = ['Item_Identifier', 'Outlet_Identifier']
+from sklearn.model_selection import cross_val_score
+def pipeline(algorithm, train, test,export_name):
+    #preparing our data
+    X_train = train[[col for col  in train.columns if col not in ['Item_Outlet_Sales']]]
+    y_train = train['Item_Outlet_Sales']
+    X_train.drop(id_col, axis = 1, inplace = True)
+    X_test = test.drop([id_col[0],id_col[1],target],axis = 1 )
+    
+    #Cross Validation
+    scores = cross_val_score(algorithm, X_train, y_train, cv = 20, scoring ='neg_mean_squared_error' )
+    scores = np.sqrt(scores)
+    
+    # Predicting test set
+    algorithm.fit(X_train, y_train)
+    y_pred_train = algorithm.predict(X_train)
+    y_pred = algorithm.predict(X_test)
+    
+    # Export submission file
+    submission = test[id_col]
+    submission['Item_Outlet_Sales'] = y_pred
+    submission.to_csv(export_name+'.csv', index = False)
+
+
+# In[117]:
+
+
+from sklearn.linear_model import LinearRegression
+algorithm = LinearRegression()
+pipeline(algorithm, train, test, 'submission_lr')
+
+
+# In[118]:
+
+
+X_train = train[[col for col  in train.columns if col not in ['Item_Outlet_Sales']]]
+y_train = train['Item_Outlet_Sales']
+X_train.drop(id_col, axis = 1, inplace = True)
+X_test = test.drop([id_col[0],id_col[1],target],axis = 1 )
+
+
+# In[120]:
+
+
+from sklearn.linear_model import LinearRegression
+algorithm = LinearRegression()
+scores = cross_val_score(algorithm, X_train, y_train, cv = 20, scoring ='neg_mean_squared_error' )
+scores = np.sqrt(np.abs(scores))
+
+
+# In[123]:
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X_train ,y_train,random_state = 10)
+algorithm = LinearRegression()
+algorithm.fit(X_train, y_train)
+y_pred = algorithm.predict(X_test)
+
+
+# In[124]:
+
+
+from sklearn import metrics
+
+
+# In[125]:
+
+
+np.sqrt(np.abs(metrics.mean_squared_error(y_test, y_pred)))
+
+
+# In[128]:
+
+
+X_test_prediction = test.drop([id_col[0],id_col[1],target],axis = 1 )
+
+
+# In[132]:
+
+
+y_pred_submission = algorithm.predict(X_test_prediction)
+
+
+# In[133]:
+
+
+y_pred_submission.shape
+
+
+# In[134]:
+
+
+submission_lr = test[id_col]
+submission_lr['Item_Outlet_Sales'] = y_pred_submission
+
+
+# In[135]:
+
+
+submission_lr.to_csv('submission_lr_trial.csv', index = False)
+
+
+# In[136]:
+
+
+submission_lr.Item_Outlet_Sales.min()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[142]:
+
+
+#Define target and ID columns:
+target = 'Item_Outlet_Sales'
+IDcol = ['Item_Identifier','Outlet_Identifier']
+from sklearn.model_selection import cross_val_score
+from sklearn import metrics
+def modelfit(alg, dtrain, dtest, predictors, target, IDcol, filename):
+    #Fit the algorithm on the data
+    alg.fit(dtrain[predictors], dtrain[target])
+        
+    #Predict training set:
+    dtrain_predictions = alg.predict(dtrain[predictors])
+
+    #Perform cross-validation:
+    cv_score = cross_val_score(alg, dtrain[predictors], dtrain[target], cv=20, scoring='neg_mean_squared_error')
+    cv_score = np.sqrt(np.abs(cv_score))
+    
+    #Print model report:
+    print ("\nModel Report")
+    print ("RMSE : %.4g" % np.sqrt(metrics.mean_squared_error(dtrain[target].values, dtrain_predictions)))
+    print ("CV Score : Mean - %.4g | Std - %.4g | Min - %.4g | Max - %.4g" % (np.mean(cv_score),np.std(cv_score),np.min(cv_score),np.max(cv_score)))
+    
+    #Predict on testing data:
+    dtest[target] = alg.predict(dtest[predictors])
+    
+    #Export submission file:
+    IDcol.append(target)
+    submission = pd.DataFrame({ x: dtest[x] for x in IDcol})
+    submission.to_csv(filename, index=False)
+
+
+# In[143]:
+
+
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+predictors = [x for x in train.columns if x not in [target]+IDcol]
+# print predictors
+alg1 = LinearRegression(normalize=True)
+modelfit(alg1, train, test, predictors, target, IDcol, 'alg1.csv')
+coef1 = pd.Series(alg1.coef_, predictors).sort_values()
+coef1.plot(kind='bar', title='Model Coefficients')
+
+
+# In[144]:
+
+
+predictors = [x for x in train.columns if x not in [target]+IDcol]
+alg2 = Ridge(alpha=0.05,normalize=True)
+modelfit(alg2, train, test, predictors, target, IDcol, 'alg2.csv')
+coef2 = pd.Series(alg2.coef_, predictors).sort_values()
+coef2.plot(kind='bar', title='Model Coefficients')
+
+
+# In[145]:
+
+
+from sklearn.tree import DecisionTreeRegressor
+predictors = [x for x in train.columns if x not in [target]+IDcol]
+alg3 = DecisionTreeRegressor(max_depth=15, min_samples_leaf=100)
+modelfit(alg3, train, test, predictors, target, IDcol, 'alg3.csv')
+coef3 = pd.Series(alg3.feature_importances_, predictors).sort_values(ascending=False)
+coef3.plot(kind='bar', title='Feature Importances')
+
+
+# In[149]:
+
+
+predictors = ['Item_MRP','Outlet_Type_Grocery Store','Outlet_Type_Supermarket Type3','Outlet_Establishment_Year']
+alg4 = DecisionTreeRegressor(max_depth=8, min_samples_leaf=150)
+modelfit(alg4, train, test, predictors, target, IDcol, 'alg4.csv')
+coef4 = pd.Series(alg4.feature_importances_, predictors).sort_values(ascending=False)
+coef4.plot(kind='bar', title='Feature Importances')
+
+
+# In[150]:
+
+
+from sklearn.ensemble import RandomForestRegressor
+predictors = [x for x in train.columns if x not in [target]+IDcol]
+alg5 = RandomForestRegressor(n_estimators=200,max_depth=5, min_samples_leaf=100,n_jobs=4)
+modelfit(alg5, train, test, predictors, target, IDcol, 'alg5.csv')
+coef5 = pd.Series(alg5.feature_importances_, predictors).sort_values(ascending=False)
+coef5.plot(kind='bar', title='Feature Importances')
+
+
+# In[151]:
+
+
+predictors = [x for x in train.columns if x not in [target]+IDcol]
+alg6 = RandomForestRegressor(n_estimators=400,max_depth=6, min_samples_leaf=100,n_jobs=4)
+modelfit(alg6, train, test, predictors, target, IDcol, 'alg6.csv')
+coef6 = pd.Series(alg6.feature_importances_, predictors).sort_values(ascending=False)
+coef6.plot(kind='bar', title='Feature Importances')
+
+
+# In[ ]:
+
+
+
+
+
+# In[157]:
+
+
+from xgboost import XGBRFRegressor
+predictors = [x for x in train.columns if x not in [target]+IDcol]
+alg7 = XGBRFRegressor()
+modelfit(alg7, train, test, predictors, target, IDcol, 'alg7.csv')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
 
 # In[ ]:
 
